af0fb6e39fc646e67e99dc68c41aab0319b936a9
NSJoker <laughofnsjoker@gmail.com>
2023-01-25 14:03:07 +0530

    Initial Commit

---
+++ PhotosPickerDemo/PhotosPickerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosPickerDemoApp.swift
+//  PhotosPickerDemo
+//
+//  Created by Chandrachudh K on 25/01/23.
+//
+
+import SwiftUI
+
+@main
+struct PhotosPickerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
